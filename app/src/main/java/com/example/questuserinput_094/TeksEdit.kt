a435cc3bfaaa208264c7554211c662faeaa322af
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-10-30 11:46:33 +0700

    Mengatur posisi column dengan vertical dan horinzontal

--- app/src/main/java/com/example/questuserinput_094/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_094/TeksEdit.kt
@@ -1,11 +1,16 @@
 package com.example.questuserinput_094
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormDataDiri(modifier: Modifier
@@ -19,6 +24,11 @@ fun FormDataDiri(modifier: Modifier
     var nama by remember {mutableStateOf("")}
     var alamat by remember {mutableStateOf("")}
     var jenis by remember {mutableStateOf("")}
-    
+
     val gender:List<String> = listOf("Laki-laki","Perempuan")
+
+    Column(modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally){
+    }
 }
