b7cbd4635ce077a1ecb4d3dba0603866ccbcf3a6
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-10-30 12:36:42 +0700

    Mengubah main activity memanggil fun FormDataDiri

--- app/src/main/java/com/example/questuserinput_094/MainActivity.kt
+++ app/src/main/java/com/example/questuserinput_094/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestUserInput_094Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    FormDataDiri(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
--- app/src/main/java/com/example/questuserinput_094/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_094/TeksEdit.kt
@@ -3,7 +3,6 @@ package com.example.questuserinput_094
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -24,10 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
-import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
