121d86d3920f7866aabbf1e2249565bb01be2f84
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-10-30 11:37:38 +0700

    Menambahkan Variabel Alamat

--- app/src/main/java/com/example/questuserinput_094/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_094/TeksEdit.kt
@@ -1,2 +1,13 @@
 package com.example.questuserinput_094
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormDataDiri(modifier: Modifier
+){
+    //Variabel-variabel untuk mengingat nilai masukan dari keyboard
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+}
